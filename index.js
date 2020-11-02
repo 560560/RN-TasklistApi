451a8645a6b3d1299d79d2429f6772ffa5e221d2
GerasimenkoAN <560560@bk.ru>
2020-11-02 13:15:16 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api", (req, res)=>{
+    res.send({method: "GET"})
+
+})
+
+app.listen(4000, () => {
+    console.log("Server is listening")
+})
