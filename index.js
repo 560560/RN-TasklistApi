481e59a0b3c01dbbbb18499fb8ee118cc74a36b2
GerasimenkoAN <560560@bk.ru>
2020-11-04 21:45:36 +0500

    Testing ...

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get("/appName", (req, res)=>{
+
+let todos = [
+    {id: "1", title: "Test todo"},
+
+]
+
+app.get("/appName", (req, res) => {
     res.send({appName: "AGRO APP"})
 
+})
+
+app.get("/todos", (req, res) => {
+    res.send(todos)
 })
 
 app.listen(4000, () => {
