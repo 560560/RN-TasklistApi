e6f1c6209a06ab87c5d30c8a8881650862fce470
GerasimenkoAN <560560@bk.ru>
2020-11-09 13:49:51 +0500

    Доработа метод Delete. На тесте..

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/todos", (req, res) => {
 app.post("/todos", (req, res) => {
     if (req.body.title) {
         let newTodo = {id: Date.now().toString(), title: req.body.title}
-        todos.unshift(newTodo)
+        todos = [newTodo, ...todos]
         res.send({
             status: 201,
             newTodo
