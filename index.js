2256d7b48b3f48e5b9b3a904ab7afad30ca8bd4d
GerasimenkoAN <560560@bk.ru>
2020-12-01 13:15:36 +0500

    Тестирую функционал изменения статуса задачи

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json())
-let todos = [{id: "1", title: "Тестовая задача"}]
+let todos = [{id: "1", title: "Тестовая задача", isDone: true}]
 
 app.get("/appName", (req, res) => {
     res.send({appName: "СПИСОК ЗАДАЧ"})
@@ -13,13 +13,33 @@ app.get("/todos", (req, res) => {
 
 app.post("/todos", (req, res) => {
     if (req.body.title) {
-        let newTodo = {id: Date.now().toString(), title: req.body.title}
+        let newTodo = {id: Date.now().toString(), title: req.body.title, isDone: false}
         console.log(todos)
         todos.unshift(newTodo)
         console.log(todos)
         res.send({
             status: 201,
             newTodo
+        })
+    }
+})
+
+app.post("/todo", (req, res) => {
+    if (req.body.id) {
+
+        todos.map(todo => {
+            if (todo.id === req.body.id) {
+                todo.isDone = !todo.isDone
+                return todo
+            } else {
+                return todo
+            }
+
+        })
+        let todoForAnswer = todos.filter(todo => todo.id === req.body.id)
+        res.send({
+            status: 201,
+            todoForAnswer
         })
     }
 })
