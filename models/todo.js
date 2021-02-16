8256591341f69423c10a19cb643b618661724114
GerasimenkoAN <560560@bk.ru>
2021-02-16 22:26:03 +0500

    Добавил Code Style и изменил механизм формирования даты при создании Задачи

--- models/todo.js
+++ models/todo.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const TodoSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    isDone: {
-        type: Boolean,
-        required: true,
-    },
-    created: {
-        type: Date,
-        required: true,
-    },
-    profileId: {
-        type: String,
-        required:true
-    },
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  isDone: {
+    type: Boolean,
+    required: true,
+  },
+  created: {
+    type: Date,
+    required: true,
+  },
+  profileId: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("todo", TodoSchema)
+module.exports = mongoose.model('todo', TodoSchema);
