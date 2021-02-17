e181b38e0eaf3d1a38c629d8e1b8fe9a6e286ad9
GerasimenkoAN <560560@bk.ru>
2021-02-17 23:18:31 +0500

    В методы работы с БД добавлена проверка по authKey

--- routes/todos.js
+++ routes/todos.js
@@ -7,6 +7,12 @@ const Todo = require('../models/todo');
 const User = require('../models/user');
 
 
+const profileIdGetter = async (authKey) => {
+  const profile = await User.findOne({'authKey': authKey});
+  return profile ? profile._id : null;
+};
+
+
 router.get('/appName', (req, res) => {
   res.send({appName: 'СПИСОК ЗАДАЧ'});
 });
@@ -25,99 +31,158 @@ router.get('/todos', async (req, res) => {
 });
 
 router.post('/todos', async (req, res) => {
-  if (req?.body?.title) {
-    const date = moment()
-    const currentDate = date.tz("Asia/Yekaterinburg")
-    let newTodo = new Todo({
-      title: req.body.title,
-      isDone: false,
-      created: currentDate,
-      profileId: uuid.v1(),
-    });
-    try {
-      const savedTodo = await newTodo.save();
+  if (req?.body?.title && req?.body?.authKey) {
+    const date = moment();
+    const currentDate = date.tz('Asia/Yekaterinburg');
+    const profileId = await profileIdGetter(req.body.authKey);
+    if (profileId) {
+      try {
+        let newTodo = new Todo({
+          title: req.body.title,
+          isDone: false,
+          created: currentDate,
+          profileId: profileId,
+        });
+        const savedTodo = await newTodo.save();
+        res.send({
+          status: "Created",
+          savedTodo: savedTodo,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
       res.send({
-        status: 201,
-        savedTodo: savedTodo,
+        status: 'Auth Fail',
       });
-    } catch (err) {
-      console.log(err);
     }
-
+  } else if (!req?.body?.title) {
+    res.send({
+      status: 'Empty title',
+    });
+  } else if (!req?.body?.authKey) {
+    res.send({
+      status: 'Empty authKey',
+    });
   }
 });
 
 router.post('/todo-done', async (req, res) => {
-  try {
-    if (req?.body?.id) {
-      const updateStatus = await Todo.updateOne({_id: req.body.id}, {isDone: req.body.isDone});
-      const modifidedTodo = await Todo.findById(req.body.id);
-      if (updateStatus.nModified) {
-        res.send({
-          status: 'Success',
-          modifidedTodo: modifidedTodo,
-        });
-      } else {
-        res.send({
-          status: 'Fail',
-        });
+  if (req?.body?.id && req?.body?.authKey) {
+    const profileId = await profileIdGetter(req.body.authKey);
+    if (profileId) {
+      try {
+        const updateStatus = await Todo.updateOne({
+          '_id': req.body.id,
+          'profileId': profileId,
+        }, {"isDone": req.body.isDone});
+        const modifidedTodo = await Todo.findById(req.body.id);
+        if (updateStatus.nModified) {
+          res.send({
+            status: 'Modified',
+            modifidedTodo: modifidedTodo,
+          });
+        } else {
+          res.send({
+            status: 'Fail',
+          });
+        }
+      } catch (err) {
+        console.log(err);
       }
+    } else {
+      res.send({
+        status: 'Auth Fail',
+      });
     }
-  } catch (err) {
-    console.log(err);
-
+  } else if (!req?.body?.id) {
+    res.send({
+      status: 'Empty id',
+    });
+  } else if (!req?.body?.authKey) {
+    res.send({
+      status: 'Empty authKey',
+    });
   }
 });
 
 router.post('/todo-edit', async (req, res) => {
-  try {
+  if (req?.body?.id && req?.body?.newTitle && req?.body?.authKey) {
+    const profileId = await profileIdGetter(req.body.authKey);
+    if (profileId) {
+      try {
+        const updateStatus = await Todo.updateOne({
+          '_id': req.body.id, 'profileId': profileId,
+        }, {"title": req.body.newTitle});
+        const modifidedTodo = await Todo.findById(req.body.id);
+        if (updateStatus.nModified) {
+          res.send({
+            status: 'Modified',
+            modifidedTodo: modifidedTodo,
+          });
+        } else {
+          res.send({
+            status: 'Fail',
+          });
+        }
 
-    if (req?.body?.id && req?.body?.newTitle) {
-      const updateStatus = await Todo.updateOne({_id: req.body.id}, {title: req.body.newTitle});
-      const modifidedTodo = await Todo.findById(req.body.id);
-
-      if (updateStatus.nModified) {
-        res.send({
-          status: 'Success',
-          modifidedTodo: modifidedTodo,
-        });
-      } else {
-        res.send({
-          status: 'Fail',
-        });
+      } catch (err) {
+        console.log(err);
       }
+    } else {
+      res.send({
+        status: 'Auth Fail',
+      });
     }
-
-
-  } catch (err) {
-    console.log(err);
-
+  } else if (!req?.body?.id) {
+    res.send({
+      status: 'Empty id',
+    });
+  } else if (!req?.body?.newTitle) {
+    res.send({
+      status: 'Empty newTitle',
+    });
+  } else if (!req?.body?.authKey) {
+    res.send({
+      status: 'Empty authKey',
+    });
   }
-
 });
 
 router.delete('/todos', async (req, res) => {
-  try {
-    if (req?.body?.id) {
-      const removedTodo = await Todo.deleteOne({_id: req.body.id});
-      if (removedTodo['deletedCount'] === 1) {
-        res.send({
-          message: `Todo with id ${req.body.id} was delete successfully`,
-          status: 'Success',
-        });
-      } else {
-        res.send({
-          message: `Todo with id ${req.body.id} was NOT deleted`,
-          status: 'Fail',
-        });
+  if (req?.body?.id && req?.body?.authKey) {
+    const profileId = await profileIdGetter(req.body.authKey);
+    if (profileId) {
+      try {
+        const removedTodo = await Todo.deleteOne({_id: req.body.id});
+        if (removedTodo['deletedCount'] === 1) {
+          res.send({
+            message: `Todo with id ${req.body.id} was delete successfully`,
+            status: 'Deleted',
+          });
+        } else {
+          res.send({
+            message: `Todo with id ${req.body.id} was NOT deleted`,
+            status: 'Fail',
+          });
+        }
+      } catch (err) {
+        console.log(err);
       }
-
+    } else {
+      res.send({
+        status: 'Auth Fail',
+      });
     }
-  } catch (err) {
-    console.log(err);
+  } else if (!req?.body?.id) {
+    res.send({
+      status: 'Empty id',
+    });
+  } else if (!req?.body?.authKey) {
+    res.send({
+      status: 'Empty authKey',
+    });
   }
-
-
 });
 
 
@@ -131,6 +196,7 @@ router.post('/register', async (req, res) => {
     });
     try {
       const savedUser = await newUser.save();
+      console.log('savedUser = ', savedUser);
       res.send({
         status: 201,
       });
@@ -147,14 +213,12 @@ router.post('/login', async (req, res) => {
           {
             'email': req.body.email,
             'password': req.body.password,
-          }, {
-            authKey: uuid.v1(),
-          });
+          }, {'authKey': uuid.v1()},
+      );
       let modifidedUser = await User.find({'email': req.body.email});
-      console.log('modifidedUser', modifidedUser);
       if (modifidedUser) {
         res.send({
-          status: 'Success',
+          status: 'Authorize success',
           authKey: modifidedUser?.[0]?.authKey,
         });
       }
